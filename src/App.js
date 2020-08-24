32fbda8404f4925d97d073b26d7c5f8186a9ca86
Moreen <moreenkim36@gmail.com>
2020-08-24 10:52:39 +0300

    Fetch content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,21 +6,14 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: '1234',
-        },
-        {
-          name: 'Dracula',
-          id: '5678',
-        },
-        {
-          name: 'Zombie',
-          id: '9012',
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
   render() {
     return (
